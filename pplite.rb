9143c5d342e5c45d569aec39a9dd9e316950eaa1
Masaki Waga <masakiwaga@gmail.com>
2021-02-17 20:27:14 +0900

    updated

--- pplite.rb
+++ pplite.rb
@@ -1,6 +1,6 @@
 class Pplite < Formula
-  desc 'C++ library implementing the abstract domain of convex polyhedra'
-  homepage 'https://www.cs.unipr.it/~zaffanella/PPLite/PPLite.html'
+  desc "C++ library implementing the abstract domain of convex polyhedra"
+  homepage "https://www.cs.unipr.it/~zaffanella/PPLite/PPLite.html"
   url "https://www.cs.unipr.it/~zaffanella/PPLite/releases/pplite-0.7.tar.gz"
   sha256 "66f5a3371fa6e91d93713f3f4eb31e1d07cc3742826565d93fff41326ffaa3cb"
   depends_on "flint" => :build
