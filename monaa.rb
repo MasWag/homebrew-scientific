f943dfa1737e37f68a180f0a8df08aee158b9d2f
Masaki Waga <MasWag@users.noreply.github.com>
2021-02-23 12:50:10 +0900

    Update monaa.rb

--- monaa.rb
+++ monaa.rb
@@ -1,10 +1,10 @@
 class Monaa < Formula
   desc 'Tool for timed patten matching with automata-based acceleration'
   homepage 'https://github.com/MasWag/monaa'
-  url 'https://github.com/MasWag/monaa/archive/0.4.1.zip'
-  version '0.4.1'
+  url 'https://github.com/MasWag/monaa/archive/0.5.1.zip'
+  version '0.5.1'
   head 'https://github.com/MasWag/monaa.git'
-  sha256 'f41b86c826d007dee15407198d1304cc93ee891987be1bfa8a1ef543e3e2a615'
+  sha256 'cb9331d63bfd38d386c68222c187bc6243685d364392af99cf59c8aa33f4847a'
   depends_on 'cmake' => :build
   depends_on 'boost' => :build
   depends_on 'eigen' => :build
@@ -15,11 +15,16 @@ class Monaa < Formula
     mkdir_p 'build'
     chdir 'build'
     system 'cmake', '..', '-DCMAKE_BUILD_TYPE=Release', "-DCMAKE_INSTALL_PREFIX=#{prefix}", *std_cmake_args
-    system 'make', 'monaa'
-    system 'make', 'install'
+    system "make", "monaa", "tre2ta"
+    system "make", "install"
+    system "install" "-m755" "tre2ta" "#{prefix}/bin"
     if File.exist? '../doc/monaa.1' then
       mkdir_p "#{prefix}/share/man/man1/"
       system 'install', '../doc/monaa.1', "#{prefix}/share/man/man1/"
+    end
+    if File.exist? "../doc/tre2ta.1" then
+      mkdir_p "#{prefix}/share/man/man1/"
+      system "install" "-m644" "../doc/tre2ta.1" "#{prefix}/share/man/man1/"
     end
   end
 
