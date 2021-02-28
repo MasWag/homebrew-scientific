f789821db61d6edd779453dc6361b85940f83106
Masaki Waga <MasWag@users.noreply.github.com>
2021-02-28 17:48:59 +0900

    Updated to monaa 0.5.2

--- monaa.rb
+++ monaa.rb
@@ -1,26 +1,26 @@
 class Monaa < Formula
-  desc 'Tool for timed patten matching with automata-based acceleration'
-  homepage 'https://github.com/MasWag/monaa'
-  url 'https://github.com/MasWag/monaa/archive/0.5.1.zip'
-  version '0.5.1'
-  head 'https://github.com/MasWag/monaa.git'
-  sha256 'cb9331d63bfd38d386c68222c187bc6243685d364392af99cf59c8aa33f4847a'
-  depends_on 'cmake' => :build
-  depends_on 'boost' => :build
-  depends_on 'eigen' => :build
-  depends_on 'bison' => :build
-  depends_on 'flex' => :build
+  desc "Tool for timed patten matching with automata-based acceleration"
+  homepage "https://github.com/MasWag/monaa"
+  url "https://github.com/MasWag/monaa/archive/0.5.2.zip"
+  version "0.5.2"
+  head "https://github.com/MasWag/monaa.git"
+  sha256 "bfbb66f2501e796c62f1028fc4df2a421c8deef4f47b90261760dd2de466e2f1"
+  depends_on "cmake" => :build
+  depends_on "boost" => :build
+  depends_on "eigen" => :build
+  depends_on "bison" => :build
+  depends_on "flex" => :build
 
   def install
-    mkdir_p 'build'
-    chdir 'build'
-    system 'cmake', '..', '-DCMAKE_BUILD_TYPE=Release', "-DCMAKE_INSTALL_PREFIX=#{prefix}", *std_cmake_args
+    mkdir_p "build"
+    chdir "build"
+    system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}", *std_cmake_args
     system "make", "monaa", "tre2ta"
     system "make", "install"
     system "install", "-m755", "tre2ta", "#{prefix}/bin"
     if File.exist? '../doc/monaa.1' then
       mkdir_p "#{prefix}/share/man/man1/"
-      system 'install', '../doc/monaa.1', "#{prefix}/share/man/man1/"
+      system "install", "../doc/monaa.1", "#{prefix}/share/man/man1/"
     end
     if File.exist? "../doc/tre2ta.1" then
       mkdir_p "#{prefix}/share/man/man1/"
