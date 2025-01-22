079553b442e291339b6df6dc8def45f0e6242653
Masaki Waga <masakiwaga@gmail.com>
2025-01-22 16:32:21 +0900

    Fixed the style

--- Casks/uppaal.rb
+++ Casks/uppaal.rb
@@ -1,11 +1,10 @@
-
 cask "uppaal" do
   version "5.0.0"
   sha256 "66dfdbd4b941fbc16a541c4c3962f9768a80a841b4bbe3815d0c31197c0c33ff"
 
   url "https://download.uppaal.org/uppaal-5.0/uppaal-#{version}/UPPAAL-#{version}-app.zip"
   name "Uppaal"
-  desc "Integrated tool environment for modeling, simulation, and verification of real-time systems"
+  desc "Tool environment for modeling, simulation, and verification of real-time systems"
   homepage "https://uppaal.org/"
 
   livecheck do
@@ -21,12 +20,12 @@ cask "uppaal" do
   app "UPPAAL-#{version}.app"
 
   postflight do
-    puts "Please register at veriaal.dk to obtain a free academic license key. See https://uppaal.org/downloads/ for details."
+    puts "Please register at veriaal.dk for a license key. See https://uppaal.org/downloads/ for details."
   end
 
   zap trash: [
     "~/Library/Preferences/org.uppaal.uppaal.plist",
-    "~/Library/Saved Application State/org.uppaal.uppaal.savedState"
+    "~/Library/Saved Application State/org.uppaal.uppaal.savedState",
   ]
 
   caveats do
