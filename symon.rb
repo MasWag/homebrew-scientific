e199b79b398c46347cc32931ab207c69dada023d
Masaki Waga <masakiwaga@gmail.com>
2025-06-13 15:11:08 +0900

    Added the formula of SyMon

---
+++ symon.rb
@@ -0,0 +1,24 @@
+class Symon < Formula
+  desc "A tool for SYmbolic MONitoring"
+  homepage "https://github.com/MasWag/symon"
+  version "0.1.0"
+  license "MIT"
+  head "https://github.com/MasWag/symon.git", branch: "new_syntax"
+
+  depends_on "boost"
+  depends_on "cmake" => :build
+  depends_on "ppl"
+  depends_on "tree-sitter"
+  depends_on "tree-sitter-symon"
+
+  def install
+    system "cmake", "-B", "build", "-S", ".", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    # Run symon
+    system bin/"symon", "--version"
+  end
+end
