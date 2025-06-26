f2128b0d8163e3e8e7d93581214e8a30c18c02af
Masaki Waga <masakiwaga@gmail.com>
2025-06-26 16:53:33 +0900

    tchecker.rb: Update to v0.8

--- tchecker.rb
+++ tchecker.rb
@@ -4,8 +4,8 @@
 class Tchecker < Formula
   desc "Open-source verification tool for timed automata"
   homepage "https://github.com/ticktac-project/tchecker"
-  url "https://github.com/ticktac-project/tchecker/archive/refs/tags/v0.2.tar.gz"
-  sha256 "a638016de673f003280b2c23185cf007992fd09e8e785779d5eeee30e52ace6b"
+  url "https://github.com/ticktac-project/tchecker/archive/refs/tags/v0.8.tar.gz"
+  sha256 "3a2ed981c5f63d8d4587d07a1c8467626bff0714f1ed089cd8d37c19a649e75d"
   license "MIT"
   head "https://github.com/ticktac-project/tchecker.git"
   depends_on "bison" => :build
