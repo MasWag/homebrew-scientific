9dfb48aa2892ff057eb2e401607f815c081c3e10
Masaki Waga <masakiwaga@gmail.com>
2026-08-23 10:45:04 +0900

    phaverlite.rb: Update to v0.8

--- phaverlite.rb
+++ phaverlite.rb
@@ -4,8 +4,8 @@
 class Phaverlite < Formula
   desc "PHAVerLite (PHAVer + PPLite) is a variant of PHAVer, a formal verification tool for computing reachability of hybrid systems."
   homepage "https://github.com/ezaffanella/PHAVerLite"
-  url "https://github.com/ezaffanella/PHAVerLite/raw/main/releases/phaverlite-0.7.tar.gz"
-  sha256 "4de2a80b11852b99590b35e3a8601b58c2612854585d5ca70ace82adf4026bb0"
+  url "https://github.com/ezaffanella/PHAVerLite/raw/main/releases/phaverlite-0.8.tar.gz"
+  sha256 "3a54705f38c56386557c2151c707e572f88b985afbc72ed9df5d384368ed6c5d"
   depends_on "pplite" => :build
 
   def install
@@ -15,7 +15,7 @@ class Phaverlite < Formula
   end
 
   test do
-    assert_equal "PHAVerLite 0.7: starting\n", `#{bin}/phaverlite -version`
+    assert_equal "PHAVerLite 0.8: starting\n", `#{bin}/phaverlite -version`
   end
 end
 
